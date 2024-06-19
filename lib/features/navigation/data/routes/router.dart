05842bca893d9bc012d7c854d5c245301150760f
Sylwia <sylwiasikora93@gmail.com>
2024-06-19 14:38:44 +0200

    Refactor BlocProviders and fix dialog context issue

--- lib/features/navigation/data/routes/router.dart
+++ lib/features/navigation/data/routes/router.dart
@@ -1,9 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fit_connect/features/home/home_screen.dart';
 import 'package:fit_connect/features/navigation/data/routes/tab_routes.dart';
 import 'package:fit_connect/features/workout_session/presentation/screens/all_workout_sessions_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../constants/constants.dart';
@@ -11,30 +9,20 @@ import '../../../../core/api/response.dart';
 import '../../../../core/api/result_status.dart';
 import '../../../../core/dependency_injection/dependency_injection.dart';
 import '../../../../core/state/app_state.dart';
-import '../../../account/presentation/bloc/user_data_bloc.dart';
-import '../../../account/presentation/bloc/user_data_event.dart';
 import '../../../account/presentation/screens/account_creation_screen.dart';
 import '../../../account/presentation/screens/account_screen.dart';
-import '../../../auth/domain/repositories/auth_repository.dart';
-import '../../../auth/presentation/bloc/auth_bloc.dart';
 import '../../../auth/presentation/screens/auth_screen.dart';
 import '../../../auth/presentation/screens/forgot_password_screen.dart';
 import '../../../auth/presentation/screens/reset_password_screen.dart';
 import '../../../common/presentation/error_screen.dart';
 import '../../../common/presentation/not_found_screen.dart';
-import '../../../explore/presentation/bloc/explore_bloc.dart';
-import '../../../explore/presentation/bloc/explore_event.dart';
 import '../../../explore/presentation/screens/explore_screen.dart';
 import '../../../explore/presentation/screens/user_profile_screen.dart';
 import '../../../messaging/data/models/conversation.dart';
-import '../../../messaging/domain/repositories/message_repository.dart';
-import '../../../messaging/presentation/bloc/message_bloc.dart';
-import '../../../messaging/presentation/bloc/message_event.dart';
 import '../../../messaging/presentation/screens/direct_message_screen.dart';
 import '../../../messaging/presentation/screens/messaging_screen.dart';
 import '../../../shared/data/models/user.dart' as customUser;
 import '../../../shared/domain/repositories/user_repository.dart';
-import '../../presentation/bloc/navigation_bloc.dart';
 import '../../presentation/screens/tab_navigator.dart';
 import 'deeplink_handler.dart';
 
@@ -81,9 +69,9 @@ class AppRoute {
           &&
           path == Routes.auth.path) {
         Response<bool> response =
-            await userRepository.hasCompletedOnboarding(appState.user?.id);
+        await userRepository.hasCompletedOnboarding(appState.user?.id);
         bool hasCompletedOnboarding =
-            response.result == ResultStatus.success ? response.data! : false;
+        response.result == ResultStatus.success ? response.data! : false;
 
         if (!hasCompletedOnboarding) {
           return Routes.onboarding.path;
@@ -104,9 +92,7 @@ class AppRoute {
       GoRoute(
         path: Routes.auth.path,
         name: Routes.auth.name,
-        builder: (context, state) => BlocProvider(
-            create: (context) => AuthBloc(getIt<AuthRepository>()),
-            child: const AuthScreen()),
+        builder: (context, state) => const AuthScreen(),
       ),
       GoRoute(
         path: "${Routes.error.path}/:error",
@@ -125,9 +111,7 @@ class AppRoute {
             String? code = state.pathParameters['code'];
             String? email = state.pathParameters['email'];
             if (code != null && email != null) {
-              return BlocProvider(
-                  create: (context) => AuthBloc(getIt<AuthRepository>()),
-                  child: ResetPasswordScreen(code: code, email: email));
+              return ResetPasswordScreen(code: code, email: email);
             } else {
               return const NotFoundScreen();
             }
@@ -135,42 +119,30 @@ class AppRoute {
       GoRoute(
         path: Routes.forgotPassword.path,
         name: Routes.forgotPassword.name,
-        builder: (context, state) => BlocProvider(
-            create: (context) => AuthBloc(getIt<AuthRepository>()),
-            child: const ForgotPasswordScreen()),
+        builder: (context, state) => const ForgotPasswordScreen(),
       ),
       GoRoute(
         path: Routes.onboarding.path,
         name: Routes.onboarding.name,
-        builder: (context, state) => BlocProvider(
-            create: (context) => UserDataBloc(
-                getIt<FirebaseAuth>(instanceName: firebaseAuthInstance),
-                getIt<UserRepository>()),
-            child: AccountCreationScreen()),
+        builder: (context, state) => AccountCreationScreen(),
       ),
       ShellRoute(
         navigatorKey: _shellNavigatorKey,
         builder: (context, state, child) {
-          return BlocProvider(
-            create: (context) => NavigationBloc(),
-            child: TabNavigator(
-              child: child,
-            ),
+          return TabNavigator(
+            child: child,
           );
         },
         routes: [
           GoRoute(
             path: Routes.home.path,
             pageBuilder: (context, state) =>
-                const NoTransitionPage(child: HomeScreen()),
+            const NoTransitionPage(child: HomeScreen()),
           ),
           GoRoute(
               path: Routes.explore.path,
-              pageBuilder: (context, state) => NoTransitionPage(
-                  child: BlocProvider(
-                      create: (context) => ExploreBloc(getIt<UserRepository>())
-                        ..add(FetchUsersEvent()),
-                      child: const ExploreScreen())),
+              pageBuilder: (context, state) => const NoTransitionPage(
+                  child: ExploreScreen()),
               routes: [
                 GoRoute(
                   path: Routes.userDetails.path,
@@ -182,47 +154,27 @@ class AppRoute {
               ]),
           GoRoute(
               path: Routes.messages.path,
-              pageBuilder: (context, state) => NoTransitionPage(
-                    child: BlocProvider(
-                        create: (context) => MessageBloc(
-                            getIt<MessageRepository>(), getIt<UserRepository>())
-                          ..add(const LoadAllConversationsEvent()),
-                        child: const MessagingScreen()),
-                  ),
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: MessagingScreen(),
+              ),
               routes: [
                 GoRoute(
                     path: Routes.directMessage.path,
                     builder: (context, state) {
                       final args = state.extra as Map<String, dynamic>;
                       final user =
-                          args[NavigationConstants.userKey] as customUser.User;
+                      args[NavigationConstants.userKey] as customUser.User;
                       final Conversation? conversation =
-                          args[NavigationConstants.conversationKey];
-                      return BlocProvider(
-                        create: (context) => MessageBloc(
-                            getIt<MessageRepository>(),
-                            getIt<UserRepository>()),
-                        child: DirectMessageScreen(
-                            otherUser: user, conversation: conversation),
-                      );
+                      args[NavigationConstants.conversationKey];
+                      return DirectMessageScreen(
+                          otherUser: user, conversation: conversation);
                     }),
               ]),
           GoRoute(
               path: Routes.account.path,
-              pageBuilder: (context, state) => NoTransitionPage(
-                    child: MultiBlocProvider(providers: [
-                      BlocProvider<UserDataBloc>(
-                        create: (context) => UserDataBloc(
-                          getIt<FirebaseAuth>(
-                              instanceName: firebaseAuthInstance),
-                          getIt<UserRepository>(),
-                        )..add(FetchUserData()),
-                      ),
-                      BlocProvider<AuthBloc>(
-                        create: (context) => AuthBloc(getIt<AuthRepository>()),
-                      ),
-                    ], child: const AccountScreen()),
-                  ),
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: AccountScreen(),
+              ),
               routes: [
                 GoRoute(
                   path: Routes.accountInfo.path,
@@ -234,7 +186,7 @@ class AppRoute {
           GoRoute(
             path: Routes.workoutSessions.path,
             pageBuilder: (context, state) =>
-                const NoTransitionPage(child: AllWorkoutSessionsScreen()),
+            const NoTransitionPage(child: AllWorkoutSessionsScreen()),
           ),
         ],
       ),
@@ -247,3 +199,4 @@ class AppRoute {
     return protectedRoutes.contains(path);
   }
 }
+
